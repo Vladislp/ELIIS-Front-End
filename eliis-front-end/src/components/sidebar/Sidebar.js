d47cd9f6ea3aa3f2b7984857da01c5cb85393f3b
Vladislp <42935979+Vladislp@users.noreply.github.com>
2023-10-19 10:19:02 +0300

    5-Fix-Side-Navigation-Bar-CSS
    * Refactor both js and css file
    * Fixed positioning problems

--- eliis-front-end/src/components/sidebar/Sidebar.js
+++ eliis-front-end/src/components/sidebar/Sidebar.js
@@ -1,25 +1,44 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import Icon from '@mdi/react';
-import { mdiViewDashboard } from '@mdi/js';
-import { mdiBookOpenVariant } from '@mdi/js';
-import { mdiCalendarCheck } from '@mdi/js';
-import { mdiMessageTextOutline } from '@mdi/js';
-import { mdiBullhornOutline } from '@mdi/js';
-import { mdiClockOutline } from '@mdi/js';
-import { mdiCalendarOutline } from '@mdi/js';
-import { mdiFolderMultipleImage } from '@mdi/js';
-import { mdiSilverware } from '@mdi/js';
-import { mdiCloud } from '@mdi/js';
-import { mdiFile } from '@mdi/js';
-import { mdiContacts } from '@mdi/js';
-import { mdiTextBoxCheck } from '@mdi/js';
-import { mdiLifebuoy } from '@mdi/js';
+import {
+  mdiViewDashboard,
+  mdiBookOpenVariant,
+  mdiCalendarCheck,
+  mdiMessageTextOutline,
+  mdiBullhornOutline,
+  mdiClockOutline,
+  mdiCalendarOutline,
+  mdiFolderMultipleImage,
+  mdiSilverware,
+  mdiCloud,
+  mdiFile,
+  mdiContacts,
+  mdiTextBoxCheck,
+  mdiLifebuoy,
+} from '@mdi/js';
 import './Sidebar.css';
 
 const Sidebar = () => {
+  const menuItems = [
+    { to: '/dashboard', icon: mdiViewDashboard, label: 'Dashboard' },
+    { to: '/diary', icon: mdiBookOpenVariant, label: 'Diary' },
+    { to: '/plans', icon: mdiCalendarCheck, label: 'Plans' },
+    { to: '/messages', icon: mdiMessageTextOutline, label: 'Messages' },
+    { to: '/announcements', icon: mdiBullhornOutline, label: 'Announcements' },
+    { to: '/workschedule', icon: mdiClockOutline, label: 'Work schedule' },
+    { to: '/events', icon: mdiCalendarOutline, label: 'Events' },
+    { to: '/gallery', icon: mdiFolderMultipleImage, label: 'Gallery' },
+    { to: '/documents', icon: mdiCloud, label: 'Documents' },
+    { to: '/foodmenu', icon: mdiSilverware, label: 'Food menu' },
+    { to: '/applications', icon: mdiFile, label: 'Applications' },
+    { to: '/contacts', icon: mdiContacts, label: 'Contacts' },
+    { to: '/surveys', icon: mdiTextBoxCheck, label: 'Surveys' },
+    { to: '/help', icon: mdiLifebuoy, label: 'Help' },
+  ];
+
   return (
-    <div className='sidebar sidebar-with-line gap-2'>
+    <div className='sidebar'>
       <nav>
         <ul className="p-4 type-menu">
           {menuItems.map((item, index) => (
@@ -39,46 +58,5 @@ const Sidebar = () => {
     </div>
   );
 };
-
-const menuItems = [
-  { 
-    to: '/dashboard', 
-    icon: mdiViewDashboard, 
-    label: 'Dashboard' 
-  },
-  { 
-    to: '/diary', 
-    icon: mdiBookOpenVariant, 
-    label: 'Diary' 
-  },
-  { 
-    to: '/plans', 
-    icon: mdiCalendarCheck, 
-    label: 'Plans' 
-  },
-  { 
-    to: '/messages', 
-    icon: mdiMessageTextOutline, 
-    label: 'Messages' 
-  },
-  { 
-    to: '/announcements', 
-    icon: mdiBullhornOutline, 
-    label: 'Announcements' 
-  },
-  { 
-    to: '/workschedule', 
-    icon: mdiClockOutline, 
-    label: 'Work schedule' 
-  },
-  { to: '/events', icon: mdiCalendarOutline, label: 'Events' },
-  { to: '/gallery', icon: mdiFolderMultipleImage, label: 'Gallery' },
-  { to: '/documents', icon: mdiCloud, label: 'Documents' },
-  { to: '/foodmenu', icon: mdiSilverware, label: 'Food menu' },
-  { to: '/applications', icon: mdiFile, label: 'Applications' },
-  { to: '/contacts', icon: mdiContacts, label: 'Contacts' },
-  { to: '/surveys', icon: mdiTextBoxCheck, label: 'Surveys' },
-  { to: '/help', icon: mdiLifebuoy, label: 'Help' },
-];
 
 export default Sidebar;
